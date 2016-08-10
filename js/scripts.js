a9860f59fcf68725c2d2bfa4ed31a2d577db202a
Amber Farrington and Ewa Manek <amber@>
2016-08-10 10:39:45 -0700

    Add functionality for subtraction, division, and multiplication and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -28,5 +28,26 @@ $(document).ready(function(){
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
     $("#output").text(result);
-});
+  });
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
 });
